285059a1dfbafd9ac37f6f5406f970f74db9bcca
AnshBansal12 <136993340+AnshBansal12@users.noreply.github.com>
2024-08-11 20:49:05 +0530

    Question

--- Question3.java
+++ Question3.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Question3 {
+import java.util.Scanner;
+public class Question3 {
+    public static void main(String[] args){
+   Scanner sc= new Scanner (System.in);
+   System.out.println("Enter the first value");
+   int a =sc.nextInt();
+        System.out.println("Enter the second value");
+   int b=sc.nextInt();
+   a=a+b;
+   b=a-b;
+
+   a=a-b;
+        System.out.println("The swapped first value is");
+        System.out.println(a);
+        System.out.println("The swapped second value is");
+        System.out.println(b);
+    }
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 8th August Questions
 ![img.png](img.png)
+Q3. ![img_1.png](img_1.png)
