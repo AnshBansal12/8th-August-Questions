491795f7ba1eddd852c799ca5f43438664ab84f9
AnshBansal12 <136993340+AnshBansal12@users.noreply.github.com>
2024-08-12 02:16:21 +0530

    Question

---
+++ Question6.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Question6 {
+    public static void main(String[] args)
+    {Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the integer value");
+        int n =sc.nextInt();
+
+        System.out.println(Integer.toString(n, 16));
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 ![img.png](img.png)
 Q3. ![img_1.png](img_1.png)
 Q5.![img.png](img.png)
+Q6.![img_2.png](img_2.png)
