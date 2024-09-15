e9e3b82fd266dd3627aa005959b38b4452f30c31
AnshBansal12 <136993340+AnshBansal12@users.noreply.github.com>
2024-09-15 20:00:21 +0530

    Question

--- Question7.java
+++ Question7.java
@@ -2,10 +2,16 @@ import java.util.Scanner;
 public class Question7 {
     public static void main(String[] arg){
         Scanner sc= new Scanner(System.in);
-        System.out.println("Enter the integer value");
-        String n =sc.next();
-        int decimal = Integer.parseInt(n, 2);
-        System.out.println(n + " in binary = " + decimal + " in decimal.");
+        System.out.println("Enter the binary no.");
+        int n =sc.nextInt();
+        int d=0,t=n,i=0;
+        while(t>0)
+        {
+            int r=t%10;
+            t/=10;
+            d+=r*Math.pow(2,i++);
+        }
+        System.out.println( " Decimal equivalent of "+n+"is" + d );
 
     }
 }
--- Question8.java
+++ Question8.java
@@ -1,12 +1,24 @@
 import java.util.Scanner;
-
+import java.lang.*;
 public class Question8 {
     public static void main(String[] args)
     {Scanner sc= new Scanner (System.in);
         System.out.println("Input any hexadecimal no.:");
-        String n = sc.next();
-        int num = Integer.parseInt(n,16);
+        String n = sc.nextLine();
+        int des=0;
+        int j=0;
+        int len=n.length();
+        for(int i=len-1;i>=0;i--)
+        {if(n.charAt(i)>='0'&& n.charAt(i)<='9')
+        {des+=((n.charAt(i)-48)*Math.pow(16,j));
+                j++;}
+            else if(n.charAt(i)>='A'&& n.charAt(i)<='F')
+            {des+=((n.charAt(i)-55)*Math.pow(16,j));
+                j++;
+            }
+        }
 
-        System.out.println("Decimal equivalent of given hex number: "+num);
+
+        System.out.println("Decimal equivalent of "+n+" number:" +des);
     }
 }
